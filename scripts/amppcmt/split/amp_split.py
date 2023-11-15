12390e2cb3f4822f180d745a6bc43f9149d6bbf0
Snaykaw <maxime.infuso@laposte.net>
2023-11-15 10:13:53 +0100

    "Modifications of the split module/corresponding."

--- scripts/amppcmt/split/amp_split.py
+++ scripts/amppcmt/split/amp_split.py
@@ -47,47 +47,85 @@ class Train_test_split:
         self.trajectory = read(trajectory, index=":")  # Read the trajectory
         self.training_set_percentage = training_set_percentage
         self.total_length = len(self.trajectory)
+        
+    def _filename_generation(self):
+        """
+        Generate the output filenames from the input filename.
+        """
+        self.test_traj_name = self.trajectory_name.split(sep=".")[0] + "_test.traj"
+        self.train_traj_name = self.trajectory_name.split(sep=".")[0] + "_train.traj"
+        
+    def traj_get_info(self):
+        """
+        Get info on the current trajectory file.
+        """
+        # General info on the trajectory
+        print("The trajectory is composed by {} frames".format(self.total_length))
+        # Setting variables
+        self.nb_training = round(self.training_set_percentage * self.total_length)
+        self.nb_test = self.total_length - self.nb_training
+        print("The number of frames in the training set is: {}".format(self.nb_training))
+        print("The number of frames in the test set is: {}".format(self.nb_test))
+        logger.debug("The number of frames in the training set is: {}".format(self.nb_training))
+        logger.debug("The number of frames in the test set is: {}".format(self.nb_test))
+        # Filenames generation
+        self._filename_generation()
+        # Printing information about outfiles
+        print("The test trajectory is saved as {}".format(self.test_traj_name))
+        logger.debug("The test trajectory is saved as {}".format(self.test_traj_name))
+        
 
     @timeit
     def split_sets_random(self):
         """
-        Split the input trajectory into training and test trajectories. Inputs are given
-        in the init function.
+        Split the input trajectory into training and test trajectories randomly. 
+        Inputs are given in the init function.
         """
-        print("The trajectory is composed by {} frames".format(self.total_length))
-        nb_training = round(self.training_set_percentage * self.total_length)
-        nb_test = self.total_length - nb_training
-        print("The number of frames in the training set is: {}".format(nb_training))
-        print("The number of frames in the test set is: {}".format(nb_test))
-        logger.debug("The number of frames in the training set is: {}".format(nb_training))
-        logger.debug("The number of frames in the test set is: {}".format(nb_test))
+        # Getting info for the current trajectory
+        # Generating filenames
+        self.traj_get_info()
 
         # Randomly selecting configurations for the test set (smaller)
-        id_test_list = random.sample(range(0, self.total_length), nb_test)
-
-        # Generating filenames
-        test_traj_name = self.trajectory_name.split(sep=".")[0] + "_test.traj"
-        train_traj_name = self.trajectory_name.split(sep=".")[0] + "_train.traj"
+        id_test_list = random.sample(range(0, self.total_length), self.nb_test)
 
         # Creating test_list_trajectory file
-        self.test_traj = Trajectory(filename=test_traj_name, mode="w")
+        self.test_traj = Trajectory(filename=self.test_traj_name, mode="w")
         for id in id_test_list:
             frame = self.trajectory[id]
             self.test_traj.write(frame)
-        print("The test trajectory is saved as {}".format(test_traj_name))
-        logger.debug("The test trajectory is saved as {}".format(test_traj_name))
-
 
         # Creating train_list_trajectory file
-        self.train_traj = Trajectory(filename=train_traj_name, mode="w")
+        self.train_traj = Trajectory(filename=self.train_traj_name, mode="w")
         for id in range(self.total_length):
             # Passing if id is in the test set
             if id in id_test_list:
                 continue
             frame = self.trajectory[id]
             self.train_traj.write(frame)
-        print("The train trajectory is saved as {}".format(train_traj_name))
-        logger.debug("The train trajectory is saved as {}".format(train_traj_name))
+        
+    def split_sets_time(self):
+        """
+        Split the input trajectory into training and test trajectories according
+        to the simulation time (i.e., the image indexes). Well suited if the
+        training set is generated from molecular dynamics. 
+        Inputs are given in the init function.
+        """
+        # Getting info for the current trajectory
+        # Generating filenames
+        self.traj_get_info()
+        
+        # Creating train_list_trajectory file
+        self.train_traj = Trajectory(filename=self.train_traj_name, mode="w")
+        for id in range(self.nb_training):
+            frame = self.trajectory[id]
+            self.train_traj.write(frame)
+        
+        # Creating test_list_trajectory file
+        self.test_traj = Trajectory(filename=self.test_traj_name, mode="w")
+        for id in range(self.nb_test):
+            id = id + self.nb_training
+            frame = self.trajectory[id]
+            self.test_traj.write(frame)
 
 
 if __name__ == "__main__":
