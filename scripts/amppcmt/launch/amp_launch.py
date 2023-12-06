07a4f94ab6545c1529ad507b3f8a4cd01532623a
Snaykaw <maxime.infuso@laposte.net>
2023-12-06 11:07:23 +0100

    Adding documentation

--- scripts/amppcmt/launch/amp_launch.py
+++ scripts/amppcmt/launch/amp_launch.py
@@ -24,7 +24,33 @@ class Launch():
         initial_calc:bool = True,
         test:bool = False,
         filename:str = 'amp_launch.py'):
+        """
         
+        Class to generate amp-atomistics inputs from jinja2 templates
+
+        Args:
+            traj_name (str): Name of the trajectory file (training set)
+            amp_param (str, optional): Name of the amp-parameters to restart
+                the calculation. Defaults to None.
+            hiddenlayers (tuple, optional): Number of hiddenlayers and neurones,
+                following the amp-atomistics format. Defaults to (10,10).
+            checkpoints (int, optional): Checkpoint frequency. 
+                Defaults to -100.
+            forces (bool, optional): Force training. If set to False,
+                the neural network is not trained with forces. 
+                Defaults to False.
+            convergence_parameters (dict, optional): Convergence parameters
+                for the lossfunction. Following the amp-atomistics format
+                Defaults to None (default parameters).
+            initial_calc (bool, optional): Set it to True if you are launching 
+                a new calculation, to false to restart a calculation. 
+                Defaults to True.
+            test (bool, optional): Used for debbuging. Set it to True to test
+                on your local computer or super computer. 
+                Defaults to False.
+            filename (str, optional): Name of the amp-atomistics generated 
+                input file. Defaults to 'amp_launch.py'.
+        """
         self.traj_name = traj_name
         self.amp_param = amp_param
         self.hiddenlayers = hiddenlayers
